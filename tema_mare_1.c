2072348442be1f323cbd0eb3a7b6c5a7a5ab3a8d
Ovidiu-Benone Chile <ben0vi@yahoo.com>
2016-10-25 11:21:00 +0300

    Removed unused commented lines, added omre comments, fixed showing the word 'stat' at the beggining of the result of the command stat

--- tema_mare_1.c
+++ tema_mare_1.c
@@ -18,19 +18,19 @@ char* longlongtoarray(long long numar)
 	int k=0,i;
 	if(numar==0)
 		sir[k++]='0';
-	while(numar!=0)
-	{
-		sir[k]=numar%10+'0';
-		numar/=10;
-		k++;
-	}
-	sir[k]='\0';
-	for(i=0;i<k/2;i++)
-	{
-		char aux=sir[i];
-		sir[i]=sir[k-1-i];
-		sir[k-i-1]=aux;
-	}
+	while(numar!=0) 			//introducerea cifrelor in sirul dorit(in ordine inversa)
+	{							//
+		sir[k]=numar%10+'0';	//
+		numar/=10;				//
+		k++;					//
+	}							///
+	sir[k]='\0';				//inchiderea sirului
+	for(i=0;i<k/2;i++) 			//oglindirea sirului
+	{							//
+		char aux=sir[i];		//
+		sir[i]=sir[k-1-i];		//
+		sir[k-i-1]=aux;			//
+	}							///
 	return sir;
 
 }
@@ -49,48 +49,49 @@ char* time_ttoarray(time_t timp)
 	strcat(sir,longlongtoarray((long long)(timp_spart->tm_min)));
 	strcat(sir,":");
 	strcat(sir,longlongtoarray((long long)(timp_spart->tm_sec)));
-	strcat(sir," +0");
-	time_t abc=time(NULL);
-  	struct tm t;
-  	localtime_r(&abc, &t);
-  	strcat(sir,longlongtoarray(t.tm_gmtoff/3600));
-	strcat(sir,"00");
+	strcat(sir," +0");								//introducerea fusului orar
+	time_t abc=time(NULL);							//
+  	struct tm t;									//
+  	localtime_r(&abc, &t);							//
+  	strcat(sir,longlongtoarray(t.tm_gmtoff/3600));	//
+	strcat(sir,"00");								//
 
 }
 void myStat(char file_path[],char rezultat[])
 {
 
-	if(access(file_path,F_OK)==-1)
+	if(access(file_path,F_OK)==-1)	//daca fisierul exista
 	{
 		sprintf(rezultat,"Fisierul \"%s\" nu exista",file_path);
 		return;
 	}
 
 	struct stat informatii;
-	if(stat(file_path,&informatii)==-1)
+	if(stat(file_path,&informatii)==-1)	//daca functioneaza functia stat din C
 	{
-		sprintf(rezultat,"Eroare la utilizarea myStat \"%s\"",file_path);
+		sprintf(rezultat,"Eroare la utilizarea stat \"%s\"",file_path);
 		return;
 	}
 
-	strcat(rezultat,"  File: `");
+	strcpy(rezultat,"");
+	strcat(rezultat,"  File: `"); //numele fisierului
 	strcat(rezultat,file_path);
 	strcat(rezultat,"'\n");
 
-	strcat(rezultat,"  Size: ");
+	strcat(rezultat,"  Size: "); //dimensiunea fisierului
 	strcat(rezultat,longlongtoarray((long long)informatii.st_size));
 	strcat(rezultat,"\t\t");
 
-	strcat(rezultat,"Blocks: ");
+	strcat(rezultat,"Blocks: "); //numarul de blocuri
 	strcat(rezultat,longlongtoarray((long long)informatii.st_blocks));
 	strcat(rezultat,"\t   ");
 
-	strcat(rezultat,"IO Block: ");
+	strcat(rezultat,"IO Block: ");	//dimensiunea preferata a blocului
 	strcat(rezultat,longlongtoarray((long long)informatii.st_blksize));
 	strcat(rezultat,"   ");
 
-	char file_access[11];
-	switch(informatii.st_mode & S_IFMT)
+	char file_access[11];					//tipul fisierului
+	switch(informatii.st_mode & S_IFMT)		////file_access[0] va fi folosit la informatiile despre accesul la fisier
 	{
 		case S_IFDIR: strcat(rezultat,"directory\n"); file_access[0]='d';break;
 		case S_IFCHR: strcat(rezultat,"character-oriented device file\n"); file_access[0]='c' ; break;
@@ -102,7 +103,7 @@ void myStat(char file_path[],char rezultat[])
 		default: strcat(rezultat,"unkown file type\n"); file_access[0]='u' ; break;
 	}
 	
-	char sir[50]="";
+	char sir[50]="";	//ID-ul device-ului in hexa, apoi in deci
 	strcat(rezultat,"Device: ");
 	sprintf(sir,"%x",(int)informatii.st_dev);
 	strcat(rezultat,sir);
@@ -110,16 +111,16 @@ void myStat(char file_path[],char rezultat[])
 	strcat(rezultat,longlongtoarray((long long)informatii.st_dev));
 	strcat(rezultat,"d\t");
 
-	strcat(rezultat,"Inode: ");
+	strcat(rezultat,"Inode: ");  //Inodul fisierului
 	strcat(rezultat, longlongtoarray((long long)informatii.st_ino));
 	strcat(rezultat,"\t   ");	
 
-	strcat(rezultat,"Links: ");
+	strcat(rezultat,"Links: ");  //numarul de linkuri
 	strcat(rezultat,longlongtoarray((long long)informatii.st_nlink));
 	strcat(rezultat,"\n");
 
-	strcat(rezultat,"Access: (0");
-	long long octal_access_mode=0;
+	strcat(rezultat,"Access: (0");  //informatiile despre accesul la fisier
+	long long octal_access_mode=0;  //va fi folosit la informatiile in octal
 	if(informatii.st_mode & S_IRUSR) {file_access[1]='r'; octal_access_mode=octal_access_mode+400;} else file_access[1]='-';
 	if(informatii.st_mode & S_IWUSR) {file_access[2]='w'; octal_access_mode=octal_access_mode+200;} else file_access[2]='-';
 	if(informatii.st_mode & S_IXUSR) {file_access[3]='x'; octal_access_mode=octal_access_mode+100;} else file_access[3]='-';
@@ -130,12 +131,12 @@ void myStat(char file_path[],char rezultat[])
 	if(informatii.st_mode & S_IWOTH) {file_access[8]='w'; octal_access_mode=octal_access_mode+  2;} else file_access[8]='-';
 	if(informatii.st_mode & S_IXOTH) {file_access[9]='x'; octal_access_mode=octal_access_mode+  1;} else file_access[9]='-';
 	file_access[10]='\0';
-	strcat(rezultat,longlongtoarray(octal_access_mode));
+	strcat(rezultat,longlongtoarray(octal_access_mode)); //informatiile in octal
 	strcat(rezultat,"/");
 	strcat(rezultat,file_access);
 	strcat(rezultat,")  ");
 
-	struct passwd *user;
+	struct passwd *user;	//Uid, impreuna cu numele user-ului ce corespunde cu Uid
 	user=getpwuid((uid_t)informatii.st_uid);
 	strcat(rezultat,"Uid: (");
 	strcat(rezultat,longlongtoarray((long long)informatii.st_uid));
@@ -146,7 +147,7 @@ void myStat(char file_path[],char rezultat[])
 		strcat(rezultat,"-");
 	strcat(rezultat,")   ");
 
-	struct group *grup;
+	struct group *grup;		//Gid, impreuna cu numele grupului ce corespunde cu Gid
 	grup=getgrgid((gid_t)informatii.st_gid);
 	strcat(rezultat,"Gid: (  ");
 	strcat(rezultat,longlongtoarray((long long)informatii.st_gid));
@@ -157,40 +158,40 @@ void myStat(char file_path[],char rezultat[])
 		strcat(rezultat,"-");
 	strcat(rezultat,")\n");
 
-	strcat(rezultat,"Access: ");
+	strcat(rezultat,"Access: ");	//Data ultimei accesari
 	strcat(rezultat,time_ttoarray(informatii.st_atime));
 	strcat(rezultat,"\n");
 
-	strcat(rezultat,"Modify: ");
+	strcat(rezultat,"Modify: ");	//Data ultimei modificari
 	strcat(rezultat,time_ttoarray(informatii.st_mtime));
 	strcat(rezultat,"\n");
 
-	strcat(rezultat,"Change: ");
+	strcat(rezultat,"Change: ");	//Data ultimei schimbari
 	strcat(rezultat,time_ttoarray(informatii.st_ctime));
 }
-void special_trim(char sir[])
+void special_trim(char sir[]) //eliminarea spatiilor inutile din sirul "sir"
 {
-	while(strchr(" \n",sir[0]))
-		memmove(sir,sir+1,strlen(sir));
-	int k=strlen(sir);
-	while(strchr(" \n",sir[k-1])&&k>0)
-	{
-		k--;
-		sir[k]='\0';
-	}
-	int i;
-	for(i=1;i<k-1;)
-	{
-		if(strchr(" \n",sir[i]) && strchr(" \n",sir[i+1]))
-		{
-			memmove(sir+i,sir+i+1,strlen(sir)-i);
-			k--;
-		}
-		else
-			i++;
-	}
+	while(strchr(" \n",sir[0])) 		//eliminarea spatiilor goale de la inceput
+		memmove(sir,sir+1,strlen(sir));	//
+	int k=strlen(sir);						//eliminarea spatiilor goale de la sfarsit
+	while(strchr(" \n",sir[k-1])&&k>0)		//
+	{										//
+		k--;								//
+		sir[k]='\0';						//
+	}										//
+	int i;													//eliminarea spatiilor duble din interior
+	for(i=1;i<k-1;)											//
+	{														//
+		if(strchr(" \n",sir[i]) && strchr(" \n",sir[i+1]))	//
+		{													//
+			memmove(sir+i,sir+i+1,strlen(sir)-i);			//
+			k--;											//
+		}													//
+		else												//
+			i++;											//
+	}														//
 }
-int count_words(char sir[])
+int count_words(char sir[]) //self-explanatory
 {
 	int i,count=0,k;
 	k=strlen(sir);
@@ -200,15 +201,16 @@ int count_words(char sir[])
 	if(k!=0) count++;
 	return count;
 }
-void manipulate(char sir[])
+void manipulate(char sir[]) //manipularea sirului primit de catre procesul fiu, "sir" primind dupa
+							//apelul manipulate(sir);rezultatul ce va fi apoi trimis catre parinte 
 {
 	if(count_words(sir)!=2)
 	{
 		strcpy(sir,"Comanda stat are nevoie de un argument. Exemplu: \"myStat file.txt\"");
 	}
-	else
+	else	//determinarea comenzii ce va trebui folosita
 	{
-		char *p, sir2[1000];
+		char *p, sir2[1000];	
 		int count=0;
 		p=strtok(sir,"\n ");
 		while(p)
@@ -240,27 +242,7 @@ void manipulate(char sir[])
 }
 int main(int argc, char* argv[])
 {
-	/*
-	if(argc != 2)
-	{
-		printf("%s\n","Comanda myStat are nevoie de un argument. Exemplu: \"myStat file.txt\" ");
-		exit(1);
-	}
-	char rezultat[2000]="";
-	myStat(argv[1],rezultat);
-	printf("%s",rezultat);
-	*/
-	
-	/////////////////////////////////
-
-	/*char sir[2000];
-	while(0 != read(0,sir,2000))
-	{
-		printf("bla\n");
-	}*/
-
-	//////////////////////////////////
-
+		
 	int pid,pipefd1[2],pipefd2[2];
 	char *sir;
 	sir=malloc(2000);
@@ -274,26 +256,26 @@ int main(int argc, char* argv[])
 		perror("pipe2");
 		exit(3);
 	}
-	switch(pid=fork())
+	switch(pid=fork())		//fork
 	{
-		case -1:printf("eroare la fork"); exit(1);
-			case 0:
+		case -1:printf("eroare la fork"); exit(1);		//eroare
+		case 0:		//procesul fiu
+		{
+			char sirDinFiu[2000];
+			int nrBytes;
+			close(pipefd1[1]);
+			close(pipefd2[0]);
+			while(0!= (nrBytes=read(pipefd1[0],sirDinFiu,2000)))
 			{
-				char sirDinFiu[2000];
-				int nrBytes;
-				close(pipefd1[1]);
-				close(pipefd2[0]);
-				while(0!= (nrBytes=read(pipefd1[0],sirDinFiu,2000)))
-				{
-					sirDinFiu[nrBytes]='\0';
-					manipulate(sirDinFiu);
-					write(pipefd2[1],sirDinFiu,strlen(sirDinFiu));
-				}
-				close(pipefd1[0]);
-				close(pipefd2[1]);
-				exit(0);
+				sirDinFiu[nrBytes]='\0';
+				manipulate(sirDinFiu);
+				write(pipefd2[1],sirDinFiu,strlen(sirDinFiu));
 			}
-		default:
+			close(pipefd1[0]);
+			close(pipefd2[1]);
+			exit(0);
+		}
+		default:   //procesul tata
 		{
 			close(pipefd1[0]);
 			close(pipefd2[1]);
@@ -309,7 +291,7 @@ int main(int argc, char* argv[])
 					write(pipefd1[1],sir,strlen(sir));
 					int nr=read(pipefd2[0],sir,2000);
 					sir[nr]='\0';
-					printf("Numar de octeti: %d \n",nr-1);
+					printf("Numar de octeti primiti: %d \n",nr-1);
 					fflush(stdout);
 					if(nr!=0)
 						{printf("%s\n",sir);fflush(stdout);}
